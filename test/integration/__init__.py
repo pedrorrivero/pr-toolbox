660b334eaf3328f39221cd414cb093b078e348e4
Pedro Rivero <pedro.rivero.ramirez@gmail.com>
2023-05-19 15:37:32 -0400

    chore: update license

--- test/integration/__init__.py
+++ test/integration/__init__.py
@@ -1,6 +1,4 @@
-# This code is part of Qiskit.
-#
-# (C) Copyright IBM 2023.
+# (C) Copyright 2023 Pedro Rivero
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
