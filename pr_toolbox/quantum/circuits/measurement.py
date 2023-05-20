f11f808f168ec8a412936292194c173fea65c829
Pedro Rivero <pedro.rivero.ramirez@gmail.com>
2023-05-20 09:16:09 -0400

    feat(quantum): add circuit measurement tools

---
+++ pr_toolbox/quantum/circuits/measurement.py
@@ -0,0 +1,20 @@
+# (C) Copyright 2023 Pedro Rivero
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Quantum circuits tools."""
+
+from __future__ import annotations
+
+from qiskit.circuit import Measure, QuantumCircuit, Qubit
+
+
+def get_measured_qubits(circuit: QuantumCircuit) -> set[Qubit]:
+    """Get qubits with at least one measurement gate in them."""
+    return {qargs[0] for gate, qargs, _ in circuit if isinstance(gate, Measure)}
