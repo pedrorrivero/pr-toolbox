f11cb582d7d3f60818f62518b26f76bef9ab80e6
Pedro Rivero <pedro.rivero.ramirez@gmail.com>
2023-05-20 17:37:41 -0400

    perf(quantum): simplify circuit permutation inference tools

--- pr_toolbox/quantum/circuits/layout.py
+++ pr_toolbox/quantum/circuits/layout.py
@@ -24,26 +24,18 @@ def infer_initial_permutation(circuit: QuantumCircuit) -> tuple[int, ...]:
     transpile_layout: TranspileLayout = circuit.layout
     if transpile_layout is None:
         return tuple(range(circuit.num_qubits))
-    num_qubits = len(transpile_layout.input_qubit_mapping)
-    input_qubit_mapping = transpile_layout.input_qubit_mapping
-    inverted_input_mapping = {i: q for q, i in input_qubit_mapping.items()}
-    input_qubits = tuple(inverted_input_mapping[i] for i in range(num_qubits))
-    initial_layout = transpile_layout.initial_layout
-    return tuple(initial_layout[q] for q in input_qubits)
+    position_qubit_mapping = {p: q for q, p in transpile_layout.input_qubit_mapping.items()}
+    num_qubits = len(position_qubit_mapping)
+    virtual_qubits = (position_qubit_mapping[p] for p in range(num_qubits))
+    return tuple(transpile_layout.initial_layout[q] for q in virtual_qubits)
 
 
-# TODO: output_qubits = circuit.qubits (simplify)
 def infer_final_permutation(circuit: QuantumCircuit) -> tuple[int, ...]:
     """Infer final transpilation permutation (i.e. from physical circuit start to end)."""
     transpile_layout: TranspileLayout = circuit.layout
-    if transpile_layout is None:
+    if transpile_layout is None or transpile_layout.final_layout is None:
         return tuple(range(circuit.num_qubits))
-    num_qubits = len(transpile_layout.input_qubit_mapping)
-    output_qubit_mapping = {q: i for i, q in enumerate(circuit.qubits)}
-    inverted_output_mapping = {i: q for q, i in output_qubit_mapping.items()}
-    output_qubits = tuple(inverted_output_mapping[i] for i in range(num_qubits))
-    final_layout = transpile_layout.final_layout or Layout(output_qubit_mapping)
-    return tuple(final_layout[q] for q in output_qubits)
+    return tuple(transpile_layout.final_layout[q] for q in circuit.qubits)
 
 
 def infer_total_permutation(circuit: QuantumCircuit) -> tuple[int, ...]:
@@ -58,9 +50,10 @@ def infer_total_permutation(circuit: QuantumCircuit) -> tuple[int, ...]:
 ################################################################################
 def infer_total_layout(circuit: QuantumCircuit) -> Layout:
     """Infer total transpilation layout (i.e. from virtual circuit to physical end)."""
-    if circuit.layout is None:
+    transpile_layout: TranspileLayout = circuit.layout
+    if transpile_layout is None:
         return Layout(dict(enumerate(circuit.qubits)))
-    initial = circuit.layout.initial_layout
-    final = circuit.layout.final_layout or Layout(dict(enumerate(circuit.qubits)))
+    initial = transpile_layout.initial_layout
+    final = transpile_layout.final_layout or Layout(dict(enumerate(circuit.qubits)))
     layout_dict = {q: final[circuit.qubits[i]] for q, i in initial.get_virtual_bits().items()}
     return Layout(layout_dict)
