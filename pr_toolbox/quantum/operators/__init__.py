90f1830e8894bb9c45daf834c3af4cb61c6dcc80
Pedro Rivero <pedro.rivero.ramirez@gmail.com>
2023-05-19 20:53:50 -0400

    feat(quantum): add pauli operators tools

---
+++ pr_toolbox/quantum/operators/__init__.py
@@ -0,0 +1,22 @@
+# (C) Copyright 2023 Pedro Rivero
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Quantum operators tools."""
+
+from .decomposition import AbelianDecomposer, NaiveDecomposer, OperatorDecomposer
+from .paulis import build_pauli_measurement, pauli_integer_mask
+
+__all__ = [
+    "AbelianDecomposer",
+    "NaiveDecomposer",
+    "OperatorDecomposer",
+    "build_pauli_measurement",
+    "pauli_integer_mask",
+]
