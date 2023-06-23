575c84b4dd0f8f8f3b2e011e6114b36751cbd05f
Ayse Kotil <Ayse.Kotil@ibm.com>
2023-06-22 22:16:35 -0400

    refactor(frequencies): generalize frequency mapping in utils

--- test/unit/quantum/results/test_frequencies.py
+++ test/unit/quantum/results/test_frequencies.py
@@ -9,14 +9,17 @@
 # that they have been altered from the originals.
 
 """Tests for frequency results tools."""
+from test import TYPES
+
 from numpy import sqrt
-from pytest import mark
+from pytest import mark, raises
 from qiskit.result import Counts, QuasiDistribution
 
 from pr_toolbox.quantum.results.frequencies import (
     bitflip_frequencies,
     bitmask_frequencies,
-    map_frequencies, convert_counts_to_quasi_dists,
+    convert_counts_to_quasi_dists,
+    map_frequencies,
 )
 
 
@@ -31,16 +34,12 @@ class TestMapFrequencies:
         [
             (Counts({}), lambda _: None, {}),
             (QuasiDistribution({}), lambda _: None, {}),
-
             (Counts({0: 1}), lambda _: 0, {0: 1}),
             (QuasiDistribution({0: 1}), lambda _: 0, {0: 1}),
-
             (Counts({0: 1}), lambda _: 1, {1: 1}),
             (QuasiDistribution({0: 1}), lambda _: 1, {1: 1}),
-
             (Counts({0: 1, 1: 1}), lambda _: 1, {1: 2}),
             (QuasiDistribution({0: 0.5, 1: 0.5}), lambda _: 1, {1: 1}),
-
             (Counts({0: 0, 1: 1}), lambda k: k + 1, {1: 0, 2: 1}),
             (QuasiDistribution({0: 0, 1: 1}), lambda k: k + 1, {1: 0, 2: 1}),
         ],
@@ -48,6 +47,12 @@ class TestMapFrequencies:
     def test_map_frequencies(self, frequencies, map, expected):
         """Test map frequencies base functionality."""
         assert map_frequencies(frequencies, map) == type(frequencies)(expected)
+
+    @mark.parametrize("frequencies", TYPES)
+    def test_wrong_frequency_type(self, frequencies):
+        """Test a non-FrequencyLike input."""
+        with raises(TypeError):
+            map_frequencies(frequencies, lambda _: None)
 
 
 class TestBitflipFrequencies:
@@ -105,7 +110,7 @@ class TestFrequencyConversion:
             Counts({0: 1, 1: 1}),
             Counts({0: 0, 1: 5}),
             Counts({12: 1, 13: 5, 14: 1}),
-            Counts({5: 6})
+            Counts({5: 6}),
         ],
     )
     def test_convert_counts_to_quasi_dists(self, counts):
@@ -115,4 +120,6 @@ class TestFrequencyConversion:
         assert quasi_dists.shots == counts.shots()
         if quasi_dists.shots:
             assert quasi_dists.stddev_upper_bound == sqrt(1 / quasi_dists.shots)
-        assert quasi_dists == {k: v / (quasi_dists.shots or 1) for k, v in counts.int_outcomes().items()}
+        assert quasi_dists == {
+            k: v / (quasi_dists.shots or 1) for k, v in counts.int_outcomes().items()
+        }
